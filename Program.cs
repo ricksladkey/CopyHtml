3e510e0325c6bb377df121238c8b5e9d2f513ae7
Rick Sladkey <rick.sladkey@gmail.com>
2013-11-21 00:28:23 -0500

    Initial revision.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClipHTML
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            List<string> lines = new List<string>();
+            for (;;)
+            {
+                string line = Console.ReadLine();
+                if (line == null)
+                    break;
+                lines.Add(line);
+            }
+            string input = string.Join(Environment.NewLine, lines);
+            HtmlFragment.CopyToClipboard(input, null, null);
+        }
+    }
+}
